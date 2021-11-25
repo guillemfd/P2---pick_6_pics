0c9c95533728be227648e90dfaa10a49280b6e28
Guillem Ferrer <guillemfd@gmail.com>
2021-11-25 18:04:55 +0100

    working into css

--- routes/favorites.js
+++ routes/favorites.js
@@ -49,22 +49,19 @@ router.post("/create/:id", async (req, res) => {  //:continent como params
 
 //---------- DELETE FAVORITE PICS BY id ----------------------------------------------------------------------------------------------------------------
 router.post("/delete/:id", async (req, res) => { 
-
-  const deletePicById = async (id)=>{
+  
     try{
-      const deletedPic = await Student.findByIdAndDelete(id, {new: true})
-      console.log("helloooo")
+        await Pic.findByIdAndDelete(req.params.id, {new: true})
+        await User.findByIdAndUpdate(req.session.loggedUser._id, {$pull: {pics: req.params.id}},);
     }catch(err){
       console.log(err)
     }
-
     res.redirect(`/favorites`)
-  }
 });
 
 // router.post("/delete/:id", async (req, res) => { 
-//   await User.findByIdAndUpdate(req.session.loggedUser._id,
-//     {$pop: {pics}},);
+  // await User.findByIdAndUpdate(req.session.loggedUser._id,
+  //   {$pop: {pics}},);
 
 //   res.redirect(`/favorites`)
 // });
