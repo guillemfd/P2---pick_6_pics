ab90ec1f59874601d805555b72bfcd4826467dc2
Guillem Ferrer <guillemfd@gmail.com>
2021-11-26 01:29:04 +0100

    CSS

--- routes/search.js
+++ routes/search.js
@@ -37,7 +37,7 @@ router.get("/america", isLoggedIn, async (req, res, next) => {
     const axiosCall = await axios(`https://api.unsplash.com/search/photos?query=america-landscape&limit=50&per_page=50&client_id=${process.env.API_KEY}&hash=${process.env.HASH}`)
     const results = axiosCall.data.results
 
-    console.log(results)
+    // console.log(results)
     res.render("continents/america.hbs", { results });
   }
   catch(err){
@@ -54,7 +54,7 @@ router.get("/antartica", isLoggedIn, async (req, res, next) => {
     const axiosCall = await axios(`https://api.unsplash.com/search/photos?query=antartica&limit=50&per_page=50&client_id=${process.env.API_KEY}&hash=${process.env.HASH}`)
     const results = axiosCall.data.results
 
-    console.log(results)
+    // console.log(results)
     res.render("continents/antartica.hbs", { results });
   }
   catch(err){
@@ -70,7 +70,7 @@ router.get("/asia", isLoggedIn, async (req, res, next) => {
     const axiosCall = await axios(`https://api.unsplash.com/search/photos?query=asia&limit=50&per_page=50&client_id=${process.env.API_KEY}&hash=${process.env.HASH}`)
     const results = axiosCall.data.results
 
-    console.log(results)
+    // console.log(results)
     res.render("continents/asia.hbs", { results });
   }
   catch(err){
@@ -86,7 +86,7 @@ router.get("/europe", isLoggedIn, async (req, res, next) => {
     const axiosCall = await axios(`https://api.unsplash.com/search/photos?query=europe&limit=50&per_page=50&client_id=${process.env.API_KEY}&hash=${process.env.HASH}`)
     const results = axiosCall.data.results
 
-    console.log(results)
+    // console.log(results)
     res.render("continents/europe.hbs", { results });
   }
   catch(err){
@@ -102,7 +102,7 @@ router.get("/oceania", isLoggedIn, async (req, res, next) => {
     const axiosCall = await axios(`https://api.unsplash.com/search/photos?query=oceania&limit=50&per_page=50&client_id=${process.env.API_KEY}&hash=${process.env.HASH}`)
     const results = axiosCall.data.results
 
-    console.log(results)
+    // console.log(results)
     res.render("continents/oceania.hbs", { results });
   }
   catch(err){
@@ -113,45 +113,3 @@ router.get("/oceania", isLoggedIn, async (req, res, next) => {
 
 
 module.exports = router;
-
-
-
-//----------------------------- create FAVORITE ---------- create FAVORITE ---------- create FAVORITE ---------- create FAVORITE --
-// const favButton = document.getElementById('favButton')
-
-// function addFav(){
-//   if(!audio.paused){
-//     favButton.style.backgroundImage = 'url(../sounds/sound_off.png)';
-//   } else {
-//     favButton.style.backgroundImage = 'url(../sounds/sound_on.png)';
-//   }
-// }
-
-
-// router.post("/create/:id", async (req, res) => {
-//   const axiosCall = await axios(
-//     `http://gateway.marvel.com/v1/public/characters/${req.params.id}?ts=1&apikey=${process.env.API_KEY}&hash=${process.env.HASH}`
-//   );
-
-//   const infoFromCharacter = axiosCall.data.data.results;
-
-//   const dataToUpload = {
-//     name: infoFromCharacter[0].name,
-//     description: infoFromCharacter[0].description,
-//     thumbnail:
-//       infoFromCharacter[0].thumbnail.path +
-//       "." +
-//       infoFromCharacter[0].thumbnail.extension,
-//     comic: [...infoFromCharacter[0].comics.items],
-//   };
-
-//   const justCreatedCharacter = await Character.create(dataToUpload);
-
-//   await User.findByIdAndUpdate(
-//     req.session.loggedUser._id,
-//     { $push: { characters: justCreatedCharacter._id } },
-//     { new: true }
-//   );
-
-//   res.redirect('/')
-// });
